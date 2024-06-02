16bbfec2dc74f7c56db131c7e8982ef599b4b9af
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-06-02 21:04:52 +0800

    Finished Linked List

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <bits/stdc++.h>
+#include <vector>
 
 template <typename T>
 class Node {
@@ -8,66 +8,182 @@ class Node {
 	public:
 	// members
 	T data;
+	Node<T>* pNextNode;
 	bool isNone;
-	Node* nextNode;
 
 	// constructor(s)
-	Node(T d, Node* n = nullptr) 
-	{
-		this->data = d;
-		this->nextNode = n;
-		this->isNone = false;
-	};
+	Node<T>():
+		data{},
+		pNextNode{nullptr},
+		isNone{true}
+	{}; 
 
-	Node(){
-		this->isNone = true;
-		this->nextNode = nullptr;
-	};
-
-	// destructor
-	~Node(){};
+	Node<T>(T d, Node* n):
+		data{d},
+		pNextNode{n},
+		isNone{false}	
+	{}
 };
 
 template <typename T>
-class linkedList {
+class LinkedList {
 
 	public:
 	// members
-	Node<T> head;
+	Node<T>* pHead;
 	
 	// constructor
-	linkedList() {
-		this->head = Node<T>();
-	};
+	LinkedList():
+		pHead{ new Node<T>{} }
+	{};
+
+	// destructor
+	~LinkedList() {
+		std::cout << "Linked List Destructor Called" << std::endl;
+		Node<T>* nextToDelete = this->pHead;
+		while(true) {
+			if (nextToDelete->pNextNode != nullptr) {
+				Node<T>* temp = nextToDelete->pNextNode;
+				delete nextToDelete;
+				nextToDelete = temp;
+			} else {
+				delete nextToDelete;
+				break;
+			}
+		}
+		std::cout << "Linked List Destructor Ended" << std::endl;
+	}
 
 	// print linked list
 	void print() {
-		if ((head.isNone)) {
+		if ((this->pHead)->isNone) {
 			std::cout << "Linked list is empty" << std::endl;
 			return;
 		}
 
-		Node<T> itr = head;
+		Node<T>* itrObj = this->pHead;
 
-		while(itr.isNone) {
-			if (itr.nextNode != nullptr) {
-				std::cout << itr.data << " --> " << std::endl;
+		while(true) {
+			if (itrObj->pNextNode != nullptr) {
+				std::cout << itrObj->data << " --> " << std::endl;
+				itrObj = itrObj->pNextNode;
 			} else {
-				std:: cout << itr.data << std::endl;
+				std:: cout << itrObj->data << std::endl;
+				break;
 			}
-
-			itr = *(itr.nextNode);
 		}
 	}
 
-	// int getLength
+	// getLength
+	int getLength(){
+		int count = 0;
+		Node<T>* itrObj = this->pHead;
+
+		while (itrObj->pNextNode != nullptr) {
+			count++;
+			itrObj = itrObj->pNextNode;
+			break;
+		}
+
+		return count;
+	}
+
+	// insert at beginning
+	void insertAtBeginning(T data){
+		Node<T> node = new Node<T>(data, this->pHead);
+		this->pHead = node;
+	}
+
+	// void insert at end
+	void insertAtEnd(T data) {
+		if ((this->pHead)->isNone) {
+			this->pHead = new Node<T>(data, nullptr);
+			return;
+		}
+
+		// create a new node
+		Node<T>* newNode  = new Node<T>(data, nullptr);
+		
+		// create an iterator
+		Node<T>* itrObj = this->pHead;
+
+		while (itrObj->pNextNode != nullptr) {
+			itrObj = itrObj->pNextNode;
+		}	
+
+		// assign address of new node into the last head.nextNode pointer
+		itrObj->pNextNode = newNode;
+	}
+
+	// insert at
+	void inserAt(int index, T data) {
+		if (index < 0 || index > this->getLength()) {
+			std::cout << "error: invalid index" << std::endl;
+			return;
+		} 
+
+		if (index == 0) {
+			this->insertAtBeginning();
+			return;
+		}
+
+		int count = 0;
+		Node<T>* itrObj = this->pHead;
+		while(true) {
+			if (count == index - 1) {
+				Node<T>* n = new Node<T>(data, &itrObj->pNextNode);
+				itrObj->pNextNode = n;
+				break;
+			}
+
+			itrObj = itrObj->nextNode;
+			count++;
+		}
+	};
+
+	// remove at
+	void removeAt(int index) {
+		if (index < 0 || index >= this->getLength()) {
+			std::cout << "error invalid index" << std::endl;
+			return;
+		}
+
+		if (index == 0) {
+			delete this->head;
+			this->head = (this->head)->nextNode;
+			return;
+		}
+
+		int count = 0;
+		Node<T>* itrObj = this->head;
+		while (true) {
+			if (count == index - 1) {
+				itrObj = (itrObj->nextNode)->nextNode;
+				break;
+			}
+
+			itrObj = itrObj->nextNode;
+			count++;
+		}
+	}
+
+	// insert values
+	void insertValues(std::vector<T>dataList) {
+		this->pHead = new Node<T>();
+		for (T d : dataList) {
+			this->insertAtEnd(d);
+		}
+	}
 };
 
+// main function
 int main() {
 	
 	std::cout << "Hello World" << std::endl;
 
-	linkedList<std::string> ll = linkedList<std::string>();
+	LinkedList<std::string> ll = LinkedList<std::string>();
+	std::vector<std::string> myValues{ "banana", "mango", "grapes", "orange" };
+	ll.insertValues(myValues);
 	ll.print();
 
 	return 0;
