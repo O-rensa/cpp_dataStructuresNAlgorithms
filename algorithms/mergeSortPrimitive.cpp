49fdf685a00ef58a55f09e2fb1f8abfb17ddc941
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-06-18 23:13:30 +0800

    Create mergeSortPrimitive.cpp

---
+++ algorithms/mergeSortPrimitive.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// function(s) prototype
+std::vector<int> mergeSort(std::vector<int> input);
+std::vector<int> mergeTwoSortedList(std::vector<int> left, std::vector<int> right);
+
+//
+// main function
+// 
+int main() {
+
+  return 0;
+}
+// 
+// main function
+//
+
+// function(s) definition
+std::vector<int> mergeSort(std::vector<int> input){
+  if (input.size() <= 1) {
+    return input;
+  };
+
+  int mid = input.size() / 2;
+
+
+
+  return input;
+};
+
+// std::vector<int> mergeTwoSortedList();
