a3ff074159f0afb7bda9adda9684412fa6d15542
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-06-19 23:16:35 +0800

    Update mergeSortPrimitive.cpp

--- algorithms/mergeSortPrimitive.cpp
+++ algorithms/mergeSortPrimitive.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "bits/stdc++.h"
 
 // function(s) prototype
 std::vector<int> mergeSort(std::vector<int> input);
-std::vector<int> mergeTwoSortedList(std::vector<int> left, std::vector<int> right);
-
+std::vector<int> mergeTwoSortedList(std::vector<int>& left, std::vector<int>& right);
+std::vector<int> slicing(std::vector<int>& vect, int x, int y);
+void printVec(std::vector<int>& vect);
 //
 // main function
 // 
 int main() {
+  std::vector<int> myVec{10,3,15,7,8,23,98,29};
+
+  std::vector<int> sortedMyVec = mergeSort(myVec);
+
+  // printVec(sortedMyVec);
 
   return 0;
 }
@@ -25,9 +32,65 @@ std::vector<int> mergeSort(std::vector<int> input){
 
   int mid = input.size() / 2;
 
+  std::cout << input.size() << std::endl;
+
+  std::vector<int> left = slicing(input, 0, mid);
+  std::vector<int> right = slicing(input, mid, input.size() - 1);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
 
 
+  return mergeTwoSortedList(left, right);
   return input;
 };
 
-// std::vector<int> mergeTwoSortedList();
+std::vector<int> mergeTwoSortedList(std::vector<int>& a, std::vector<int>& b){
+  std::vector<int> sortedList{};
+
+  int lenA = a.size();
+  int lenB = b.size();
+
+  int i = 0;
+  int j = 0;
+
+  while ((i < lenA) && (j < lenB)) {
+    if (a[i] <= b[j]) {
+      sortedList.push_back(a[i++]);
+    } else {
+      sortedList.push_back(b[j++]);
+    }
+  }
+
+  while (i < lenA) {
+    sortedList.push_back(a[i++]);
+  }
+
+  while (j < lenB) {
+    sortedList.push_back(b[j++]);
+  }
+
+  return sortedList;
+}
+
+__attribute__((always_inline)) inline
+std::vector<int> slicing(std::vector<int>& vect, int x, int y){
+  auto start = vect.begin() + x;
+  auto end = vect.begin() + y + 1;
+
+  std::vector<int> result(y - x + 1);
+  std::copy(start, end, result.begin());
+  return result;
+};
+
+__attribute__((always_inline)) inline
+void printVec(std::vector<int>& vect) {
+  std::cout << "{ ";
+  for (auto iter = vect.begin(); iter != vect.end(); iter++) {
+    if (iter != vect.end() - 1) {
+      std:: cout << *iter << ", ";
+    } else {
+      std::cout << *iter << " }" << std::endl;
+    }
+  }
+};
